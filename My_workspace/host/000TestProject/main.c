b6196d0447276b28312ba1aac0641c4d1276067c
Rui Mendes <ruimiguelhortasilva@gmail.com>
2025-07-04 18:11:26 +0200

    TestProject

---
+++ My_workspace/host/000TestProject/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Jun 16, 2025
+ *      Author: Rui
+ */
+
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Hello World\n");
+
+	printf("Press 'Enter' to exit this application");
+
+	getchar();
+
+	return 0;
+}
